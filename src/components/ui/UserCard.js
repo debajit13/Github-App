fd2c5e8b7efaeff909b80f5100ffb9ec0fff5060
Debajit Mallick <42708550+debajit13@users.noreply.github.com>
2022-01-31 00:17:28 +0530

    add api dadta to user card

---
+++ src/components/ui/UserCard.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Card, CardBody } from "reactstrap";
+
+const UserCard = ({ user }) => {
+  return (
+    <Card className="text-center mt-3 mb-4">
+      <img
+        src={user.avatar_url}
+        className="img-thumbnail img-rounded"
+        alt={`${user.name}'s avatar`}
+      />
+      <CardBody>
+        <div className="text-primary">{user.name}</div>
+        <div className="text-primary">{user.location}</div>
+        <div className="text-primary">{user.bio}</div>
+        <div className="text-primary">
+          Available for hire: {user.hireable ? "YES" : "NO"}
+        </div>
+        <div className="text-primary">Followers: {user.followers}</div>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default UserCard;
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,10 +1,52 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import axios from "axios";
+import { Row, Container, Col, Input, Button, InputGroup } from "reactstrap";
+import UserCard from "../components/ui/UserCard";
+import Repos from "../components/ui/Repos";
+import { Navigate } from "react-router-dom";
+import { UserContext } from "../context/UserContext";
+import { toast } from "react-toastify";
 
 const Home = () => {
+  const context = useContext(UserContext);
+  const [query, setQuery] = useState("");
+  const [user, setUser] = useState(null);
+
+  const fetchDetails = async () => {
+    try {
+      const response = await axios.get(`https://api.github.com/users/${query}`);
+      setUser(response.data);
+      console.log(response.data);
+    } catch (error) {
+      toast("Not able to locate user", { type: "error" });
+    }
+  };
+
+  // Put any page behind login
+  if (!context.user?.uid) {
+    return <Navigate to="/signin" />;
+  }
+
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <Container>
+      <Row className="mt-3">
+        <Col md="5">
+          <InputGroup>
+            <Input
+              type="text"
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
+              placeholder="Please provide the username"
+            />
+            <Button onClick={fetchDetails} color="primary">
+              Fetch User
+            </Button>
+          </InputGroup>
+          {user ? <UserCard user={user} /> : null}
+        </Col>
+        <Col md="7">{user ? <Repos repos_url={user.repos_url} /> : null}</Col>
+      </Row>
+    </Container>
   );
 };
 
