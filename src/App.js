c8e666a5b99aba72054777d895653e9b4a225d44
Debajit Mallick <42708550+debajit13@users.noreply.github.com>
2022-01-31 00:25:18 +0530

    remove Link from import in app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import React, { useState } from "react";
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useState } from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 // react-router
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // toast
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.min.css";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
 
 // firebase
-import firebase from "firebase/compat/app";
-import "firebase/compat/auth";
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
 
 // components
-import Home from "./views/Home";
-import Signin from "./views/Signin";
-import Signup from "./views/Signup";
-import NotFound from "./views/NotFound";
-import { UserContext } from "./context/UserContext";
-import Footer from "./components/layouts/Footer";
-import Header from "./components/layouts/Header";
-import firebaseConfig from "./config/firebaseConfig";
+import Home from './views/Home';
+import Signin from './views/Signin';
+import Signup from './views/Signup';
+import NotFound from './views/NotFound';
+import { UserContext } from './context/UserContext';
+import Footer from './components/layouts/Footer';
+import Header from './components/layouts/Header';
+import firebaseConfig from './config/firebaseConfig';
 
 //initialize firebase
 firebase.initializeApp(firebaseConfig);
@@ -33,10 +33,10 @@ function App() {
       <UserContext.Provider value={{ user, setUser }}>
         <Header />
         <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/signin" element={<Signin />}></Route>
-          <Route path="/signup" element={<Signup />}></Route>
-          <Route path="*" element={<NotFound />}></Route>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/signin' element={<Signin />}></Route>
+          <Route path='/signup' element={<Signup />}></Route>
+          <Route path='*' element={<NotFound />}></Route>
         </Routes>
         <Footer />
       </UserContext.Provider>
